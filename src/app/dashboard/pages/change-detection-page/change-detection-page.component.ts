6c6722c834337cb0693b76976326cbd057f39932
Jhonny <jhonnycgarcia@gmail.com>
2024-10-20 22:57:41 -0500

    ChangeDetection - OnPush

--- src/app/dashboard/pages/change-detection-page/change-detection-page.component.ts
+++ src/app/dashboard/pages/change-detection-page/change-detection-page.component.ts
@@ -1,11 +1,47 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
+import { TitleComponent } from '@shared/title/title.component';
 
 @Component({
   standalone: true,
-  imports: [],
-  templateUrl: './change-detection-page.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [
+    CommonModule,
+    TitleComponent
+  ],
+  template: `
+    <shared-title [title]="currentFrameWork()" />
+
+    <pre>{{ frameWorkAsSignal() | json }}</pre>
+    <pre>{{ frameWorkAsProperty | json }}</pre>
+  `,
   styles: ``
 })
 export default class ChangeDetectionPageComponent {
+
+  public currentFrameWork = computed(() => `Change Detection: ${this.frameWorkAsSignal().name}`);
+
+  public frameWorkAsSignal = signal({
+    name: 'Angular',
+    releaseDate: 2016,
+  });
+
+  public frameWorkAsProperty = {
+    name: 'Angular',
+    releaseDate: 2016,
+  };
+
+  constructor() {
+    setTimeout(() => {
+
+      // this.frameWorkAsProperty.name = 'React';
+      this.frameWorkAsSignal.update((value) => ({
+        ...value,
+        name: 'React',
+      }));
+
+      console.log('Hecho');
+    }, 3000);
+  }
 
 }

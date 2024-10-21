65705acfca55fef3555be5d5f4e524043dba12b2
Jhonny <jhonnycgarcia@gmail.com>
2024-10-20 22:33:18 -0500

    Control Flow - @Switch

--- src/app/dashboard/pages/control-flow-page/control-flow-page.component.ts
+++ src/app/dashboard/pages/control-flow-page/control-flow-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, signal } from '@angular/core';
 
+type Grade = 'A' | 'B' | 'F';
+
 @Component({
   standalone: true,
   imports: [],
@@ -9,6 +11,8 @@ import { Component, signal } from '@angular/core';
 export default class ControlFlowPageComponent {
 
   public showContent = signal(false);
+  public grade = signal<Grade>('A');
+
 
   public toggleContent() {
     this.showContent.update((value) => !value);

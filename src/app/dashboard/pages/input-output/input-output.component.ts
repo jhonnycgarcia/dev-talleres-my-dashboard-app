a99659ed8c59795004ba0ec02d4ff774c9043772
Jhonny <jhonnycgarcia@gmail.com>
2024-10-22 01:12:30 -0500

    Añadir productos dínamicamente

--- src/app/dashboard/pages/input-output/input-output.component.ts
+++ src/app/dashboard/pages/input-output/input-output.component.ts
@@ -1,6 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, DestroyRef, inject, signal } from '@angular/core';
+
 import { ProductCardComponent } from './ui/product-card/product-card.component';
+import { Product } from '@interfaces/product';
+import { interval, take, tap } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   standalone: true,
@@ -9,5 +13,35 @@ import { ProductCardComponent } from './ui/product-card/product-card.component';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class InputOutputComponent {
+
+  private destroyRef$ = inject(DestroyRef);
+
+  public products = signal<Product[]>([
+    {
+      id: 1,
+      name: 'Producto 1',
+      quantity: 0,
+    },
+    {
+      id: 2,
+      name: 'Producto 2',
+      quantity: 0,
+    },
+  ]);
+
+  private intervalSubscription = interval(1000).pipe(
+    tap(() => {
+      this.products.update((products) => [
+        ...products,
+        {
+          id: products.length + 1,
+          name: 'Producto ' + (products.length + 1),
+          quantity: 0,
+        }
+      ])
+    }),
+    take(7),
+    takeUntilDestroyed(this.destroyRef$)
+  ).subscribe();
 
 }

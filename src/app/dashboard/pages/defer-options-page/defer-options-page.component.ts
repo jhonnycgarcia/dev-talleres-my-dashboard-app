80887da2ae104afe68a38efe11e8b0b25d8c8585
Jhonny <jhonnycgarcia@gmail.com>
2024-10-21 19:39:17 -0500

    Defer Triggers - Interacciones.

--- src/app/dashboard/pages/defer-options-page/defer-options-page.component.ts
+++ src/app/dashboard/pages/defer-options-page/defer-options-page.component.ts
@@ -1,11 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { HeavyLoadersFastComponent } from '@shared/heavy-loaders/heavy-loaders-fast.component';
+import { TitleComponent } from '@shared/title/title.component';
 
 @Component({
   standalone: true,
-  imports: [],
-  templateUrl: './defer-options-page.component.html',
-  styles: ``
+  imports: [
+    CommonModule,
+    HeavyLoadersFastComponent,
+    TitleComponent,
+  ],
+  templateUrl: './defer-options-page.component.html'
 })
 export default class DeferOptionsPageComponent {
+
+  constructor() { }
 
 }
--- src/app/shared/heavy-loaders/heavy-loaders-fast.component.ts
+++ src/app/shared/heavy-loaders/heavy-loaders-fast.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'shared-heavy-loaders-fast',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule ],
   template: `
-    <h1>Hola Mundo</h1>
+    <section [ngClass]="['w-full', cssClass]">
+      <ng-content></ng-content>
+    </section>
   `,
   styles: ``
 })
 export class HeavyLoadersFastComponent {
+
+  @Input({ required: true })
+  public cssClass!: string;
+
+  constructor() {
+    console.log('HeavyLoadersFastComponent');
+  }
 
 }

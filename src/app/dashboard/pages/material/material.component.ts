3647939630e7760e73d597a6fcf31d2cb21bc26d
Jhonny <jhonnycgarcia@gmail.com>
2024-10-22 23:08:13 -0500

    User componentes de Angular Material

--- src/app/dashboard/pages/material/material.component.ts
+++ src/app/dashboard/pages/material/material.component.ts
@@ -1,9 +1,18 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-material',
   standalone: true,
-  imports: [],
+  imports: [
+    MatBadgeModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSlideToggleModule,
+  ],
   templateUrl: './material.component.html',
   styleUrl: './material.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
